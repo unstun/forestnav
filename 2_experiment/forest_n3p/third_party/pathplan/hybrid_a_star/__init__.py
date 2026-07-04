bb8b9a126d2e94039148d6c729b0866fe5cb4938
Sun <sun@machine-dog.local>
2026-07-03 21:25:15 -0700

    auto-backup before edit

--- 2_experiment/forest_n3p/third_party/pathplan/hybrid_a_star/__init__.py
+++ 2_experiment/forest_n3p/third_party/pathplan/hybrid_a_star/__init__.py
@@ -1,4 +1,12 @@
 from .planner import HybridAStarPlanner, Node
 from .lo_planner import LOHybridAStarPlanner
+from .operators import AnalyticExpansionOperator, AnalyticExpansionResult, DangRsOperator
 
-__all__ = ["HybridAStarPlanner", "LOHybridAStarPlanner", "Node"]
+__all__ = [
+    "AnalyticExpansionOperator",
+    "AnalyticExpansionResult",
+    "DangRsOperator",
+    "HybridAStarPlanner",
+    "LOHybridAStarPlanner",
+    "Node",
+]
